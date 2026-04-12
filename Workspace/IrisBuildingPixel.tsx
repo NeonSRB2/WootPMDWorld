caee45e82548fffd9c04db33f58ab3d6d58f9760
NeonSRB2 <118679101+NeonSRB2@users.noreply.github.com>
2026-04-12 10:46:07 -0400

    Initial Commit

---
+++ Workspace/IrisBuildingPixel.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.1" name="IrisBuildingPixel" tilewidth="8" tileheight="8" tilecount="750" columns="30">
+ <image source="IrisBuildingPixel.png" width="240" height="204"/>
+</tileset>
---
+++ Workspace/TreasureTownBase.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.1" name="TreasureTownBase" tilewidth="8" tileheight="8" tilecount="1080" columns="36">
+ <image source="TreasureTownTiles.png" width="288" height="240"/>
+</tileset>
